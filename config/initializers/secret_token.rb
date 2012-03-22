5e553afa4d5382f39c6edcbf7f661f93238c90af
Stanislav Senotrusov <stan@senotrusov.com>
2012-03-22 18:42:25 +0900

    Fresh Rails 3.2.2 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+March::Application.config.secret_token = 'f44b9f4b0372afe9071479459b19faf44f4da05eec64a870e81e5fd037a8872c2aedd0c737c19a54877c0934c5e1096510acb3b180a51aeb64f652c35c9933ef'
